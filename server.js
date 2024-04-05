6981dff1cdbee0b10d706f4fb2d2e251548f2821
jwyoohk <122680488+jwyoohk@users.noreply.github.com>
2024-04-05 10:55:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
 //  res.setHeader('bar');
-  res.end('Hello World!');
+  res.end('Hello k8s World!');
 }).listen(80);
